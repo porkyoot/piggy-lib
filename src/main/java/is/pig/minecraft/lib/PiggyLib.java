0a0ed04f0ff812bb2fd5dbd1184b679de91894ea
Porkyoot <85759819+porkyoot@users.noreply.github.com>
2026-03-20 13:02:56 +0100

    Adding autoparkour cheat feature

--- src/main/java/is/pig/minecraft/lib/PiggyLib.java
+++ src/main/java/is/pig/minecraft/lib/PiggyLib.java
@@ -11,5 +11,12 @@ public class PiggyLib implements ModInitializer {
     @Override
     public void onInitialize() {
         LOGGER.info("Initializing Piggy Lib");
+        
+        is.pig.minecraft.lib.features.CheatFeatureRegistry.register(
+                new is.pig.minecraft.lib.features.CheatFeature(
+                        "auto_parkour",
+                        "Auto Parkour",
+                        "Automatically place blocks below you when running and jumping",
+                        false));
     }
 }
